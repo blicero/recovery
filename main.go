2eeacf4b2b4f777fad9b1a49f5e15dfd02f1f475
Benjamin Walkenhorst <krylon@posteo.de>
2022-03-22 15:41:06 +0100

    Add main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+// /home/krylon/go/src/github.com/blicero/recovery/main.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 22. 03. 2022 by Benjamin Walkenhorst
+// (c) 2022 Benjamin Walkenhorst
+// Time-stamp: <2022-03-22 15:39:39 krylon>
+
+package recovery
+
+import (
+	"fmt"
+
+	"github.com/blicero/recovery/common"
+)
+
+func main() {
+	fmt.Printf("%s: Implement me!\n",
+		common.AppName)
+}
